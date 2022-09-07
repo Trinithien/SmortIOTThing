55e13cc98362a8e67fb525e0542fa5392e9f2519
Triny <Trinithien@gmail.com>
2022-09-07 08:44:26 +0200

    Add Dependency Injection

--- SmortIOTThing.Desktop/SmortIOTThing.Desktop/App.xaml.cs
+++ SmortIOTThing.Desktop/SmortIOTThing.Desktop/App.xaml.cs
@@ -6,6 +6,7 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Shapes;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.IO;
