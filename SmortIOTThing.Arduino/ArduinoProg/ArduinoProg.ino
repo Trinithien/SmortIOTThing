0c6a59f38b83f687b86d0e757177eb75b5428a54
Triny <Trinithien@gmail.com>
2022-09-07 11:14:24 +0200

    Serial com example

--- SmortIOTThing.Arduino/ArduinoProg/ArduinoProg.ino
+++ SmortIOTThing.Arduino/ArduinoProg/ArduinoProg.ino
@@ -1,9 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(10000);
 }
-
+byte b[3];
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(Serial.available()==3)
+  {
+    Serial.readBytes(b,3);
+    
+    Serial.write(b,3);
+  }
 }
