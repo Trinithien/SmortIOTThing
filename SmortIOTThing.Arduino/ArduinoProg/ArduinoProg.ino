24c7ab34fb6227de246508a4aca2639e057fbbb7
Martin Simonsen <martinsimonsen@live.no>
2022-09-21 09:09:02 +0200

    Create program for reading temprature, gives alarm when high or cold

--- SmortIOTThing.Arduino/ArduinoProg/ArduinoProg.ino
+++ SmortIOTThing.Arduino/ArduinoProg/ArduinoProg.ino
@@ -1,14 +1,36 @@
+const int hot = 25; //hot parameter
+const int cold = 20; //cold parameter
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(10000);
+  pinMode(A0, INPUT); //tmpSensor
+  pinMode(2, OUTPUT); //redLedDiode
+  pinMode(3, OUTPUT); //redLedDiode
+  Serial.begin(9600);
 }
-byte b[3];
-void loop() {
-  // put your main code here, to run repeatedly:
-  if(Serial.available()==3)
-  {
-    Serial.readBytes(b,3);
-    
-    Serial.write(b,3);
+
+void tempSensor() {
+  float sensor = analogRead(A0); //Reads the value of the analog pin
+  float voltage = (sensor/1024)*5000; //Sensor output range is 0.1-2.0V, 1024bits over 1.9V and times 5 to get a range between 0-5volt
+  float tempC = (voltage-500)/10; //To convert from voltage to temprature
+  
+  Serial.print("The temprature is: "); //Alarm for too cold temp
+  if (tempC < cold) { //cold
+    digitalWrite(2, HIGH); 
+    digitalWrite(3, LOW);
+    Serial.println(tempC);
   }
+  else if (tempC >= hot) { //Alarm for too hot temp
+    digitalWrite(2, LOW);
+    digitalWrite(3, HIGH);
+    Serial.println(tempC);
+  }
+  else { //fine
+    digitalWrite(2, LOW); //Temp is perfect
+    digitalWrite(3, LOW);
+    Serial.println(tempC);
+  }
+  delay(1000);
+}
+
+void loop(){
+  tempSensor();
 }
