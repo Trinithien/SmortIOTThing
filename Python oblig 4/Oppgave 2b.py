4cc50c332485d7c7a8787ace03512ba57313acf8
Triny <Trinithien@gmail.com>
2022-10-07 14:46:25 +0200

    Update oppgave 2b and add word doc

--- Python oblig 4/Oppgave 2b.py
+++ Python oblig 4/Oppgave 2b.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 fig = plt.figure(figsize =(10, 10))
 ax = fig.add_subplot()
-t = np.linspace(-5, 5, 100)
+t = np.linspace(-1, 1, 100)
 x = t  
 y = t**2 #Since (x,y) go goes thru the point (-1,1), (0,0) and (1,1) 
 #the formula for y will by y**2.
